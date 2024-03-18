5207eeda053d6c31aa6dbf1594816ae3f7808b6b
christophe dastrois <dastrois@users.noreply.github.com>
2024-03-18 15:01:48 +0100

    pull some changes (#3)
    * change function signature
    
    * null safe equals

--- src/main/java/com/example/demo/app/impl/CustomerSync.java
+++ src/main/java/com/example/demo/app/impl/CustomerSync.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Component
 @Validated
@@ -58,7 +57,7 @@ public class CustomerSync implements com.example.demo.app.ICustomerSync {
 
     private void populateShoppingList(ExternalCustomer externalCustomer, Customer customer) {
         log.debug("populate the shopping list");
-        List<ShoppingList> sl = new ArrayList<>();
+        ArrayList<ShoppingList> sl = new ArrayList<>();
         for (ExternalShoppingItem consumerShoppingItem : externalCustomer.getShoppingLists()) {
             ShoppingList si = new ShoppingList();
             si.setCustomer(customer);
@@ -68,8 +67,8 @@ public class CustomerSync implements com.example.demo.app.ICustomerSync {
         customer.setShoppingLists(sl);
     }
 
-    private Customer updateCustomer(Customer customer) {
-        return this.customerDataAccess.upSaveCustomer(customer);
+    private void updateCustomer(Customer customer) {
+        this.customerDataAccess.upSaveCustomer(customer);
     }
 
     private void updateDuplicate(ExternalCustomer externalCustomer, Customer duplicate) {
