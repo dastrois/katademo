cbe4bacc5566f06fa6fc4b11eb4276a8f9b4fe46
Christophe Dastrois <dac@it-guide.be>
2024-03-18 13:39:30 +0100

    reformat

--- src/main/java/com/example/demo/app/services/ICustomerDataAccess.java
+++ src/main/java/com/example/demo/app/services/ICustomerDataAccess.java
@@ -1,13 +1,12 @@
 package com.example.demo.app.services;
 
-
 import com.example.demo.model.constant.CustomerType;
 import com.example.demo.model.dao.Customer;
 import com.example.demo.model.exception.ConflictException;
 import com.example.demo.model.vo.CustomerMatches;
 
 public interface ICustomerDataAccess {
-    CustomerMatches loadCustomerCompany(String externalId,String companyNumber) throws ConflictException;
+    CustomerMatches loadCustomerCompany(String externalId, String companyNumber) throws ConflictException;
 
     CustomerMatches loadCustomer(String externalId, CustomerType type, String companyNumber) throws ConflictException;
 
